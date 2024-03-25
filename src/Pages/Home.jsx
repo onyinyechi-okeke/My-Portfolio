ebc7d9aeda4728deed13b60bfa306d589fd1f054
itsjustonyii <onyinyechiokeke733@gmail.com>
2024-03-25 13:46:17 +0100

    responsiveness 2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <div className=' bg-gradient-to-b from-gray-950 to-black w-full md:h-screen border-b-2 border-gray-500'>
         <div className='max-w-screen-lg mx-auto flex  items-center flex-col-reverse justify-center h-full px-4 md:flex-row md:pt-14'>
             <div className='flex flex-col justify-center h-full pl-4 sm:pl-0'>
-                <h2 className='text-amber-400 text-3xl sm:text-5xl font-bold pt-3 md:pt-0'>A Passionate Frontend Developer</h2>
+                <h2 className='text-amber-400 text-3xl sm:text-5xl font-bold pt-5 md:pt-0'>A Passionate Frontend Developer</h2>
                 <p className='py-5 md:pr-16'>
                 I am a highly motivated frontend developer with a passion for creating friendly, interactive and user
                 centric products. I am seeking opportunities to leverage my skills as a react developer to
@@ -16,7 +16,7 @@ function Home() {
             
 
             <div>
-                <button className='group w-fit gap-2 bg-gradient-to-r from-amber-400 to-amber-600 px-6 py-3 my-2 flex items-center rounded-md cursor-pointer text-xl'>
+                <button className='group w-fit mb-6 md:mb-0 gap-2 bg-gradient-to-r from-amber-400 to-amber-600 px-6 py-3 my-2 flex items-center rounded-md cursor-pointer text-xl'>
                     Portfolio
                     <span className='animate-pulse group-hover:rotate-90 duration-300'><FaLongArrowAltRight size={25}/></span>
                 </button>
@@ -25,7 +25,7 @@ function Home() {
 
           <figure className='pt-20 md:pt-0'>
             <img src={Onyii} alt='my picture'
-            className='rounded-2xl mx-auto w-2/3 md:w-full mt-16 md:mt-0' />
+            className='rounded-2xl mx-auto w-2/3 md:w-full mt-2 md:mt-0' />
           </figure>
       
         </div>
