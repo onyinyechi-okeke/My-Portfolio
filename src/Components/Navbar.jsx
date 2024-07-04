3aeec6b632b6ec953f622fee87b95b8f66f32127
itsjustonyii <onyinyechiokeke733@gmail.com>
2024-07-04 19:12:55 +0100

    z-index increment

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -41,7 +41,7 @@ function Navbar() {
   }
   
   return (
-    <div className='flex justify-between bg-black w-full h-20 items-center px-2 md:px-6 fixed'>
+    <div className='flex justify-between bg-black w-full h-20 items-center px-2 md:px-6 fixed z-50'>
       <div>
         <h1 className='font-signature text-5xl font-semibold ml-2'>
           Onyiicodes</h1>
